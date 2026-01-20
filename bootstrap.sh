79b9c579ed5ccafb7144eb877b2c8f8514593afc
Your Name <you@example.com>
2026-01-20 03:52:15 +0000

    update absolute path

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "Run protoc"
-cd ./common
-protoc --cpp_out=../ns3-sionna/lib/ message.proto
-protoc --python_out=../sionna_server/ message.proto
+echo "Run protoc"
+cd ./common
+protoc --cpp_out=../ns3-sionna/lib/ message.proto
+protoc --python_out=../sionna_server/ message.proto
 
-echo "Create symlink"
-cd ../ns-allinone-3.46.1/ns-3.46.1/scratch/
-ln -s ../../../ns3sionna/ns3-sionna/ .
+echo "Create symlink"
+cd /ns3-workspace/scratch/
+ln -s /ns3-workspace/contrib/ns3sionna/ns3-sionna/ .
 
-cd ..
+cd ..
+ 
